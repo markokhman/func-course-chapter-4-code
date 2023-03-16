c808e0bb741a1efc32b0ad8c97a78831bac9bc99
Mark Okhman <mark@okhmans.pro>
2023-03-16 11:26:43 +0100

    Lesson 2

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -9,15 +9,24 @@ describe("main.fc contract tests", () => {
     const blockchain = await Blockchain.create();
     const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
 
+    const initAddress = await blockchain.treasury("initAddress");
+
     const myContract = blockchain.openContract(
-      await MainContract.createFromConfig({}, codeCell)
+      await MainContract.createFromConfig(
+        {
+          number: 0,
+          address: initAddress.address,
+        },
+        codeCell
+      )
     );
 
     const senderWallet = await blockchain.treasury("sender");
 
-    const sentMessageResult = await myContract.sendInternalMessage(
+    const sentMessageResult = await myContract.sendIncrement(
       senderWallet.getSender(),
-      toNano("0.05")
+      toNano("0.05"),
+      1
     );
 
     expect(sentMessageResult.transactions).toHaveTransaction({
@@ -29,5 +38,6 @@ describe("main.fc contract tests", () => {
     const data = await myContract.getData();
 
     expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+    expect(data.number).toEqual(1);
   });
 });
--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -9,36 +9,59 @@ import {
   SendMode,
 } from "ton-core";
 
+export type MainContractConfig = {
+  number: number;
+  address: Address;
+};
+
+export function mainContractConfigToCell(config: MainContractConfig): Cell {
+  return beginCell()
+    .storeUint(config.number, 32)
+    .storeAddress(config.address)
+    .endCell();
+}
+
 export class MainContract implements Contract {
   constructor(
     readonly address: Address,
     readonly init?: { code: Cell; data: Cell }
   ) {}
 
-  static createFromConfig(config: any, code: Cell, workchain = 0) {
-    const data = beginCell().endCell();
+  static createFromConfig(
+    config: MainContractConfig,
+    code: Cell,
+    workchain = 0
+  ) {
+    const data = mainContractConfigToCell(config);
     const init = { code, data };
     const address = contractAddress(workchain, init);
 
     return new MainContract(address, init);
   }
 
-  async sendInternalMessage(
+  async sendIncrement(
     provider: ContractProvider,
     sender: Sender,
-    value: bigint
+    value: bigint,
+    increment_by: number
   ) {
+    const msg_body = beginCell()
+      .storeUint(1, 32) // OP code
+      .storeUint(increment_by, 32) // increment_by value
+      .endCell();
+
     await provider.internal(sender, {
       value,
       sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().endCell(),
+      body: msg_body,
     });
   }
 
   async getData(provider: ContractProvider) {
-    const { stack } = await provider.get("get_the_latest_sender", []);
+    const { stack } = await provider.get("get_contract_storage_data", []);
     console.log(stack);
     return {
+      number: stack.readNumber(),
       recent_sender: stack.readAddress(),
     };
   }
